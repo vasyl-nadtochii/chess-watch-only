24d79caa7e063ce1cd47baa21307f333a8bb2d37
Vasyl Nadtochii <vasyanadtochiy146@gmail.com>
2024-07-07 17:09:30 +0300

    Initial Commit

---
+++ ChessOnlyWatchOS Watch App/ChessOnlyWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessOnlyWatchOSApp.swift
+//  ChessOnlyWatchOS Watch App
+//
+//  Created by Vasyl Nadtochii on 07.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChessOnlyWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
